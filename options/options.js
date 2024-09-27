b0f7052792ce5d2d7eb72e96342aedb6c834156f
Crowdbotics <team@crowdbotics.com>
2024-09-27 16:36:43 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "chameleonpathways-49515",
+  url: "https://chameleonpathways-49515.botics.co",
+  api: "https://chameleonpathways-49515.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
